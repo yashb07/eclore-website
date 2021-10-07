0fa8d7f86fa748826f6d31edde559593439fb11d
Yash <testemail@address.com>
2021-10-07 10:33:23 +0530

    Eclore Initial Commit - Netlify
    Hosted on Netlify, initial commit for Eclore website.

---
+++ backToTop.js
@@ -0,0 +1,17 @@
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} 
